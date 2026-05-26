ee84269376de2f4ea896e72a481dfaebbab38844
FinnHornhoover <finnhornhoover@gmail.com>
2026-05-26 20:53:06 +0300

    top badge ui fixes

--- site/src/components/EntityPageSkeleton.tsx
+++ site/src/components/EntityPageSkeleton.tsx
@@ -8,13 +8,13 @@ export default function EntityPageSkeleton() {
         <Skeleton width={140} height={14} />
       </p>
       <h1><Skeleton width="60%" height={32} /></h1>
-      <p className="entity-meta">
-        <Skeleton width={48} height={48} />
-        {' '}
-        <Skeleton width={120} height={20} />
-        {' '}
-        <Skeleton width={80} height={20} />
-      </p>
+      <div className="entity-header">
+        <div className="entity-header-icon"><Skeleton width={48} height={48} /></div>
+        <div className="entity-meta">
+          <Skeleton width={120} height={20} />
+          <Skeleton width={80} height={20} />
+        </div>
+      </div>
       <Skeleton style={{ display: 'block', width: '100%', height: 120, margin: 'var(--space-4) 0' }} />
       <Skeleton style={{ display: 'block', width: '100%', height: 80, margin: 'var(--space-3) 0' }} />
       <Skeleton style={{ display: 'block', width: '100%', height: 60, margin: 'var(--space-3) 0' }} />
